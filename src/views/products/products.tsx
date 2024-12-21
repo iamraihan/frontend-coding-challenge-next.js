0e9147252f7aeb60d52ee862aff35252d5e1f5b6
Raihan Ali <trueraihan@gmail.com>
2024-12-21 23:06:01 +0600

    feat: modal persist problem is solved

--- src/views/products/products.tsx
+++ src/views/products/products.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { Product } from "@/types";
 import { ProductModal } from "@/views/products/productModal/productModal";
 import { BackToHome } from "@/components/backToHome/backToHome";
@@ -8,6 +8,7 @@ import { ProductList } from "@/views/products/productList/productList";
 import { PaginationControls } from "@/views/products/paginationControls/paginationControls";
 import { usePagination } from "@/hooks/usePagination";
 import { PRODUCTS_DATA } from "@/data/productsData";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export const Products: React.FC = () => {
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
@@ -18,14 +19,31 @@ export const Products: React.FC = () => {
     handlePageChange,
   } = usePagination({ items: PRODUCTS_DATA, itemsPerPage: 5 });
 
-  const handleOpenModal = useCallback((product: Product) => {
-    setSelectedProduct(product);
-  }, []);
+  const router = useRouter();
+
+  const handleOpenModal = useCallback(
+    (product: Product) => {
+      router.push(`/products?product-id=${product.id}`);
+    },
+    [router]
+  );
 
   const handleCloseModal = useCallback(() => {
     setSelectedProduct(null);
-  }, []);
+    router.push("/products");
+  }, [router]);
 
+  const searchParams = useSearchParams();
+  const productId = searchParams.get("product-id");
+
+  useEffect(() => {
+    if (productId) {
+      const product = PRODUCTS_DATA.find((product) => product.id === productId);
+      if (product) {
+        setSelectedProduct(product);
+      }
+    }
+  }, [productId]);
   return (
     <div>
       <BackToHome />
